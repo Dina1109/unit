066dc9aef6ec3bbc25dbc16b6501e03502302fc7
Dina-72 <amenova1109@yandex.ru>
2023-08-09 21:23:38 +0500

    changed testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,12 +7,23 @@ import static org.testng.AssertJUnit.assertEquals;
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldTest() {
+    public void shouldTestPasses() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
         int actual = service.remain(amount);
         int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldTestNotPasses() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
