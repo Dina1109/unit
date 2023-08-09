6650c48d1eed9d1b82a76d66f5e76df4a4a7c5c0
Dina-72 <amenova1109@yandex.ru>
2023-08-09 21:17:34 +0500

    changed junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,12 +8,23 @@ import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldTest() {
+    public void shouldTestPasses() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
         int actual = service.remain(amount);
         int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldTestNotPasses() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
